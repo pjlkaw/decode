3b3e94f4844e85d5bedf196874776f53750a50fd
Pedro Bispo <pedrorochabispo@gmail.com>
2025-05-14 23:20:37 -0300

    Código Alfabético 100%

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 
-
 function main_card() {
     const title_cards = document.querySelectorAll('.card h2')
     const selected_card = document.getElementById('main_card')
@@ -20,4 +19,44 @@ function main_card() {
             selected_card.innerHTML = card_content
         })
     })
+}
+
+//copiar resultado
+function copy_result() {
+    navigator.clipboard.writeText(document.getElementById('result').textContent)
+        .then(() => alert("Texto copiado!"))
+        .catch(() => alert("Falha ao copiar o texto"));
+}
+
+
+function btn_send() {
+    //leitura do card selecionado após o clique, ler o h2 do card main e pegar o valor dele
+    const code_type = document.querySelector('.main_card h2').textContent;
+    //input do usuario
+    const user_text = document.getElementById('input').value;
+
+    //Código Alfabético
+    if (code_type == "Código Alfabético") {
+        const result = user_text
+        .toLowerCase()
+        .split('')
+        .map(char => {
+            const cripto = char.charCodeAt(0)
+            if (cripto >= 97 && cripto <= 122) {
+                return cripto - 96
+            }
+            else {
+                return char;
+            }
+        })
+        .join(' ');
+        document.getElementById('result').textContent = result
+        document.getElementById('copy_result').style.display = "block";
+    }
+
+    //Base64
+
+
+
+
 }
